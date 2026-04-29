46876d82f4aff3f77b18917e5dacd364bed8888c
GuilhermeFeijo <guilhermezfeijo99@gmail.com>
2026-04-29 11:28:25 -0300

    feat: exportação e separação em lista

--- clona_pppoe/clona_pppoe.py
+++ clona_pppoe/clona_pppoe.py
@@ -1,19 +1,83 @@
 import secrets
 import paramiko
-
-host = secrets.host
-port = secrets.port
-user = secrets.user
-password = secrets.password
+import sys
 
 ssh = paramiko.SSHClient()
 ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
-ssh.connect(host, port=port, username=user, password=password)
+hostOrigem = secrets.hostOrigem
+portOrigem = secrets.portOrigem
+userOrigem = secrets.userOrigem
+passwordOrigem = secrets.passwordOrigem
 
-stdin, stdout, stderr = ssh.exec_command("ppp secret export")
+hostDestino = secrets.hostDestino
+portDestino = secrets.portDestino
+userDestino = secrets.userDestino
+passwordDestino = secrets.passwordDestino
 
-print(stdout.read().decode())
-print(stderr.read().decode())
+export = "ppp secret export"
 
-ssh.close()
+def carregar_ssh(host, port, user, password, command):
+    
+    ssh.connect(host, port=port, username=user, password=password)
+
+    stdin, stdout, stderr = ssh.exec_command(command)
+
+    retorno = stdout.read().decode()
+
+    ssh.close()
+
+    return retorno
+
+def parse_ppp_secrets(export):
+    linhas = export.splitlines()
+    comandos = []
+    buffer = ""
+
+    for linha in linhas:
+        linha = linha.strip()
+
+        # ignora comentários e vazio
+        if not linha or linha.startswith("#"):
+            continue
+
+        # junta linhas quebradas com "\"
+        if linha.endswith("\\"):
+            buffer += linha[:-1] + " "
+            continue
+        else:
+            buffer += linha
+
+        # só pega comandos add
+        if buffer.startswith("add"):
+            comandos.append(f"/ppp secret {buffer.strip()}")
+
+        buffer = ""
+
+    return comandos
+
+def main():
+
+    #exporta configuração
+    retornoExport = carregar_ssh(hostOrigem, portOrigem, userOrigem, passwordOrigem, export)
+
+    importar=parse_ppp_secrets(retornoExport)
+
+    print("Exportação ok")
+    print(importar)
+
+    #importa configuração
+    #resultado = carregar_ssh(hostDestino, portDestino, userDestino, passwordDestino, importar)
+
+    #print("Importação")
+    #print(resultado)
+
+    return 0
+
+if __name__ == "__main__":
+    try:
+        sys.exit(main())
+    except KeyboardInterrupt:
+        sys.exit(0)
+    except Exception as e:
+        sys.exit(1)
